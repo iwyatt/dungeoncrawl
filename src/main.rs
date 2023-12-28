5c2351a98095f8ef45c41b17c0118dba0625c86e
iwyatt <sayhello+git@isaacwyatt.com>
2023-12-28 12:51:29 -0800

    init pp 235

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+mod map;
+mod map_builder;
+mod player;
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+}
+
+use prelude::*;
+
+// cre
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: map_builder.map,
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+# Dungeon Crawl
+An exercise tutorial from _Hands on Rust_ by Herbery Wolverson
+
+# Credits
+The dungeon floor, wall, and adventurer graphics were kindly provided by Buch for free. Potion and scroll graphics are from Melissa Krautheim's _Fantasy Magic Set_. Weaponry is from Melle's _Fantasy Sword Set_. Monster graphics are from the game _Dungeon Crawl Stone Soup_ (CC0 license), packaged by Chris Hammons. Pp. 234 of Hands on Rust by Herbert Wolverson
