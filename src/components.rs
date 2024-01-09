18f10e7f0a3cc20e03abdb56fbd915932a5ed059
iwyatt <sayhello+git@isaacwyatt.com>
2024-01-08 22:35:27 -0800

    move to ECS system

---
+++ src/components.rs
@@ -0,0 +1,12 @@
+//use bracket_lib::terminal::FontCharType;
+pub use crate::prelude::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Player;
