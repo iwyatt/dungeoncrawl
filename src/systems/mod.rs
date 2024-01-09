0219ff891019b1ccf163def6b5ae6b64edbb81fe
iwyatt <sayhello+git@isaacwyatt.com>
2024-01-09 14:23:15 -0800

    impl randomly moving mobs

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,14 +2,16 @@ mod entity_render;
 mod map_render;
 mod player_input;
 mod collisions;
+mod random_move;
 use crate::prelude::*;
 
-use self::entity_render::entity_render;
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(collisions::collisions_system())
+        .flush() // Waits for executing systems to complete, and the flushes all outstanding system command buffers.
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(random_move::random_move_system())
         .build()
 }
