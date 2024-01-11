16d0c5e06d12c3b52b4d85aef96d43a0b2cb1379
iwyatt <sayhello+git@isaacwyatt.com>
2024-01-10 21:16:41 -0800

    impl turn based w messages

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@ mod collisions;
 mod end_turn;
 mod entity_render;
 mod map_render;
+mod movement;
 mod player_input;
 mod random_move;
 use crate::prelude::*;
@@ -28,6 +29,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -38,6 +41,8 @@ pub fn build_player_scheduler() -> Schedule {
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
+        .flush()
+        .add_system(movement::movement_system())
         .flush()
         .add_system(collisions::collisions_system())
         .flush()
